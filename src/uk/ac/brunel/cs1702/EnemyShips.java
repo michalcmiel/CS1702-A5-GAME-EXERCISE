7acb7111b1538e7bd1e1d120f07dfa5df40968a3
1707076 <1707076@brunel.ac.uk>
2018-01-24 21:18:42 +0000

    enemyships class added

---
+++ src/uk/ac/brunel/cs1702/EnemyShips.java
@@ -0,0 +1,53 @@
+package uk.ac.brunel.cs1702;
+
+public class EnemyShips {
+	public static boolean enemyFront(int shipx, int shipy){
+		switch (GalaxyExplorer.dir){
+		case 0:
+			if (GalaxyExplorer.grid[shipx][shipy+1] == 1){
+				GalaxyExplorer.returning(shipx, shipy+1);
+				return true;
+			}else return false;
+		case 1:
+			if (GalaxyExplorer.grid[shipx][shipy-1] == 1){
+				GalaxyExplorer.returning(shipx, shipy-1);
+				return true;
+			}else return false;
+		case 2:
+			if (GalaxyExplorer.grid[shipx+1][shipy] == 1){
+				GalaxyExplorer.returning(shipx+1, shipy);
+				return true;
+			}else return false;
+		case 3:
+			if (GalaxyExplorer.grid[shipx-1][shipy] == 1){
+				GalaxyExplorer.returning(shipx-1, shipy);
+				return true;
+			}else return false;
+		}
+		return false;
+	}	
+	public static boolean enemyBack(int shipx, int shipy){
+		if (GalaxyExplorer.dir ==0){
+			if (GalaxyExplorer.grid[shipx][shipy-1] == 1){
+				GalaxyExplorer.returning(shipx, shipy-1);
+				return true;
+			}else return false;
+		}else if (GalaxyExplorer.dir == 1){
+			if (GalaxyExplorer.grid[shipx][shipy+1] == 1){
+				GalaxyExplorer.returning(shipx, shipy+1);
+				return true;
+			}else return false;
+		}else if (GalaxyExplorer.dir == 2){
+			if (GalaxyExplorer.grid[shipx-1][shipy] == 1){
+				GalaxyExplorer.returning(shipx-1, shipy);
+				return true;
+			}else return false;
+		}else if (GalaxyExplorer.dir == 3){
+			if (GalaxyExplorer.grid[shipx+1][shipy] == 1){
+				GalaxyExplorer.returning(shipx+1, shipy);
+				return true;
+			}else return false;
+		}
+		return false;	
+	}
+}
